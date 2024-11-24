9eb334ab24d77ace109d15998dc710aeff3294fe
Philobater samir <philobatersamir79@gmail.com>
2024-11-24 10:56:51 +0200

    install firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:taxi_go_user_version/Core/Firebase/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:taxi_go_user_version/taxi_go_user_edition_app.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const TaxiGoUserEditionApp());
 }
