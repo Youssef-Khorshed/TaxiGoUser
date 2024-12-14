a0f274a4099083db448ceb6db762c5c38da6794e
AQ <cshazemelwazery@gmail.com>
2024-12-14 19:46:19 +0200

    added localization

--- lib/taxi_go_user_edition_app.dart
+++ lib/taxi_go_user_edition_app.dart
@@ -1,13 +1,30 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:taxi_go_user_version/Core/Utils/Routing/app_routes.dart';
+
+import 'Core/Utils/localization/cubit/local_cubit.dart';
 
 class TaxiGoUserEditionApp extends StatelessWidget {
   const TaxiGoUserEditionApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    return  LayoutBuilder(
+        builder: (context, constraints) => ScreenUtilInit(
+            designSize: constraints.maxWidth >= 600
+                ? const Size(200, 912)
+                : constraints.maxWidth < 390
+                ? const Size(490, 912)
+                : const Size(390, 844),
+            ensureScreenSize: true,
+            minTextAdapt: true,
+            builder: (context, child) => BlocProvider(
+            create: (context) => LocalCubit(),
+    child: BlocBuilder<LocalCubit, LocalState>(
+    builder: (context, state) {
     return MaterialApp(
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
@@ -16,6 +33,10 @@ class TaxiGoUserEditionApp extends StatelessWidget {
       onGenerateRoute: AppRoutes.generateRoute,
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
+    );
+  })
+            ),
+        ),
     );
   }
 }
