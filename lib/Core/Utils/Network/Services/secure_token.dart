49ecb529b6a713ab2bed7fde201264dcaa741ad0
ahmedmsaaid <ahmedmsaaid908@gmail.com>
2024-12-17 01:50:50 +0200

    fix responsive

--- lib/Core/Utils/Network/Services/secure_token.dart
+++ lib/Core/Utils/Network/Services/secure_token.dart
@@ -1,19 +1,17 @@
-import 'package:taxi_go_user_version/Core/Utils/Network/Services/api_constant.dart';
 import 'package:taxi_go_user_version/Core/Utils/Network/Services/secure_storage_helper.dart';
 import 'package:taxi_go_user_version/Core/app_constants.dart';
 
 abstract class SecureToken {
- static SecureStorageHelper secureStorageHelper = SecureStorageHelper();
-static  addToken(String token) async {
+  static SecureStorageHelper secureStorageHelper = SecureStorageHelper();
+  static addToken(String token) async {
     await secureStorageHelper.writeValue(AppConstants.kToken, token);
   }
 
- static Future<String?> getToken() async {
- return   await secureStorageHelper.readValue(AppConstants.kToken);
+  static Future<String?> getToken() async {
+    return await secureStorageHelper.readValue(AppConstants.kToken);
   }
 
-static   deleteToken() async {
+  static deleteToken() async {
     await secureStorageHelper.deleteValue(AppConstants.kToken);
-
   }
 }
