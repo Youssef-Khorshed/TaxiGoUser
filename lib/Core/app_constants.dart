db8c833a48684cd1e8b75c6e7f2fc9fb724da46b
AQ <cshazemelwazery@gmail.com>
2024-12-16 12:25:35 +0200

    added token

--- lib/Core/app_constants.dart
+++ lib/Core/app_constants.dart
@@ -1,6 +1,7 @@
 abstract class AppConstants{
   static String kToken="kToken";
-  static String kTokenValue="10|HjxvdTymwFRhqtxLfSR8nXS97VrmPFJINAFzbMzq8147c9e5";
+  //65
+  static String kTokenValue="31|Kmu10EvtFiRiyrsufe3dy2z7q5ECODM9pqhECxtP7f2800da";
 
 
 }
