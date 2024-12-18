2026bf8cf513421b2c0bc9c616aa2165fc23169b
mohamedmagdi6 <mohamedmagdii137@gmail.com>
2024-12-18 15:51:40 +0200

    add lotties

--- lib/Core/Utils/Assets/lottie/lottie.dart
+++ lib/Core/Utils/Assets/lottie/lottie.dart
@@ -1,4 +1,6 @@
 class AppLottie {
   static const String internetFailure = 'assets/lottie/internetFailure.json';
   static const String errorFailure = 'assets/lottie/errorFailure.json';
+  static const String failedLocation =
+      'assets/lottie/Animation - 1734525383447.json';
 }
