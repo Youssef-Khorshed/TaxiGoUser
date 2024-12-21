cb30ad6dbad12733e118363c4850e4d304b379c4
mohamedmagdi6 <mohamedmagdii137@gmail.com>
2024-12-21 14:27:03 +0200

    refactor FavoriteDataModel

--- lib/Features/Favourite/data/favorite_data_model.dart
+++ lib/Features/Favourite/data/favorite_data_model.dart
@@ -1,7 +1,7 @@
 class FavoriteDataModel {
   bool? success;
   String? message;
-  List<Data>? data;
+  List<FavoriteData>? data;
 
   FavoriteDataModel({this.success, this.message, this.data});
 
@@ -9,136 +9,64 @@ class FavoriteDataModel {
     success = json['success'];
     message = json['message'];
     if (json['data'] != null) {
-      data = <Data>[];
+      data = <FavoriteData>[];
       json['data'].forEach((v) {
-        data!.add(Data.fromJson(v));
+        data!.add(FavoriteData.fromJson(v));
       });
     }
   }
 }
 
-class Data {
-  int? id;
-  int? customerId;
-  String? addressFrom;
-  String? latFrom;
-  String? lngFrom;
-  String? addressTo;
-  String? latTo;
-  String? lngTo;
-  int? tripType;
-  String? paymentMethod;
-  Null salePercentage;
-  Null promocode;
-  Null promocodeId;
-  String? status;
-  String? createdAt;
-  String? updatedAt;
-  List<Ride>? ride;
+class FavoriteData {
+  Ride? ride;
 
-  Data(
-      {this.id,
-      this.customerId,
-      this.addressFrom,
-      this.latFrom,
-      this.lngFrom,
-      this.addressTo,
-      this.latTo,
-      this.lngTo,
-      this.tripType,
-      this.paymentMethod,
-      this.salePercentage,
-      this.promocode,
-      this.promocodeId,
-      this.status,
-      this.createdAt,
-      this.updatedAt,
-      this.ride});
+  FavoriteData({this.ride});
 
-  Data.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    customerId = json['customer_id'];
-    addressFrom = json['address_from'];
-    latFrom = json['lat_from'];
-    lngFrom = json['lng_from'];
-    addressTo = json['address_to'];
-    latTo = json['lat_to'];
-    lngTo = json['lng_to'];
-    tripType = json['trip_type'];
-    paymentMethod = json['payment_method'];
-    salePercentage = json['sale_percentage'];
-    promocode = json['promocode'];
-    promocodeId = json['promocode_id'];
-    status = json['status'];
-    createdAt = json['created_at'];
-    updatedAt = json['updated_at'];
-    if (json['ride'] != null) {
-      ride = <Ride>[];
-      json['ride'].forEach((v) {
-        ride!.add(Ride.fromJson(v));
-      });
-    }
+  FavoriteData.fromJson(Map<String, dynamic> json) {
+    ride = json['ride'] != null ? Ride.fromJson(json['ride']) : null;
   }
 }
 
 class Ride {
-  int? id;
-  int? rideRequestId;
-  int? captainId;
-  String? status;
   int? distance;
-  double? time;
-  String? distancePrice;
-  String? timePrice;
-  Null discount;
   String? total;
   String? rate;
-  String? paid;
-  Null remainningCustomer;
-  Null remainningCaptain;
-  String? pickupTime;
-  String? arivalTime;
-  String? createdAt;
-  String? updatedAt;
+  RideRequestData? rideRequest;
 
-  Ride(
-      {this.id,
-      this.rideRequestId,
-      this.captainId,
-      this.status,
-      this.distance,
-      this.time,
-      this.distancePrice,
-      this.timePrice,
-      this.discount,
-      this.total,
-      this.rate,
-      this.paid,
-      this.remainningCustomer,
-      this.remainningCaptain,
-      this.pickupTime,
-      this.arivalTime,
-      this.createdAt,
-      this.updatedAt});
+  Ride({this.distance, this.total, this.rate, this.rideRequest});
 
   Ride.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    rideRequestId = json['ride_request_id'];
-    captainId = json['captain_id'];
-    status = json['status'];
     distance = json['distance'];
-    time = json['time'];
-    distancePrice = json['distance_price'];
-    timePrice = json['time_price'];
-    discount = json['discount'];
+
     total = json['total'];
     rate = json['rate'];
-    paid = json['paid'];
-    remainningCustomer = json['remainning_customer'];
-    remainningCaptain = json['remainning_captain'];
-    pickupTime = json['pickup_time'];
-    arivalTime = json['arival_time'];
-    createdAt = json['created_at'];
-    updatedAt = json['updated_at'];
+
+    rideRequest = json['ride_request'] != null
+        ? RideRequestData.fromJson(json['ride_request'])
+        : null;
+  }
+}
+
+class RideRequestData {
+  int? id;
+  String? latFrom;
+  String? lngFrom;
+  String? latTo;
+  String? lngTo;
+
+  RideRequestData({
+    this.id,
+    this.latFrom,
+    this.lngFrom,
+    this.latTo,
+    this.lngTo,
+  });
+
+  RideRequestData.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    latFrom = json['lat_from'];
+    lngFrom = json['lng_from'];
+    latTo = json['lat_to'];
+    lngTo = json['lng_to'];
   }
 }
---
+++ lib/Features/Favourite/trip_favourite_widget/custom_favorite_adress_row.dart
@@ -0,0 +1,145 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
+// import 'package:geocode/geocode.dart';
+// import 'package:shimmer/shimmer.dart';
+// import 'package:taxi_go_user_version/Core/Utils/Assets/icons/app_icons.dart';
+// import 'package:taxi_go_user_version/Features/Favourite/data/favorite_data_model.dart';
+// import 'package:taxi_go_user_version/Features/History/data/history_data_model.dart';
+// import 'package:taxi_go_user_version/Features/History/history_widgets/trip_details%5B1%5D.dart';
+// import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+// class CustomFavBuildAddressRow extends StatelessWidget {
+//   const CustomFavBuildAddressRow({
+//     super.key,
+//     required this.rideDetails,
+//   });
+//   final FavoriteRideData rideDetails;
+
+//   @override
+//   Widget build(BuildContext context) {
+//     if (rideDetails.latFrom == null ||
+//         rideDetails.lngFrom == null ||
+//         rideDetails.latTo == null ||
+//         rideDetails.lngTo == null) {
+//       return Center(child: Text("Invalid location data"));
+//     }
+
+//     return FutureBuilder<Map<String, String>>(
+//       future: _getFormattedAddresses(
+//         fromLatitude: double.parse(rideDetails.latFrom!),
+//         fromLongitude: double.parse(rideDetails.lngFrom!),
+//         toLatitude: double.parse(rideDetails.latTo!),
+//         toLongitude: double.parse(rideDetails.lngTo!),
+//       ),
+//       builder: (context, snapshot) {
+//         if (snapshot.connectionState == ConnectionState.waiting) {
+//           return ListTile(
+//             title: Column(
+//               crossAxisAlignment: CrossAxisAlignment.start,
+//               children: [
+//                 Shimmer.fromColors(
+//                   baseColor: Colors.grey[300]!,
+//                   highlightColor: Colors.grey[100]!,
+//                   child: TripDetailsMap(
+//                     address: " ",
+//                     location: " ",
+//                     icon: AppIcons.iconsMapRed,
+//                   ),
+//                 ),
+//                 SizedBox(height: 10),
+//                 Shimmer.fromColors(
+//                   baseColor: Colors.grey[300]!,
+//                   highlightColor: Colors.grey[100]!,
+//                   child: TripDetailsMap(
+//                     address: " ",
+//                     location: " ",
+//                     icon: AppIcons.iconsMapBlue,
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           );
+//         }
+
+//         if (snapshot.hasError || !snapshot.hasData) {
+//           return ListTile(
+//             title: Text("حدث خطأ أثناء جلب البيانات"),
+//           );
+//         }
+
+//         final fromAddress = snapshot.data!['fromAddress']!;
+//         final fromCity = snapshot.data!['fromCity']!;
+//         final toAddress = snapshot.data!['toAddress']!;
+//         final toCity = snapshot.data!['toCity']!;
+
+//         return ListTile(
+//           title: Column(
+//             crossAxisAlignment: CrossAxisAlignment.start,
+//             children: [
+//               TripDetailsMap(
+//                 address: fromAddress,
+//                 location: fromCity,
+//                 icon: AppIcons.iconsMapRed,
+//               ),
+//               TripDetailsMap(
+//                 address: toAddress,
+//                 location: toCity,
+//                 icon: AppIcons.iconsMapBlue,
+//               ),
+//             ],
+//           ),
+//           trailing: Text(
+//             '${rideDetails.ride![0].distance} ${AppLocalizations.of(context)!.km}',
+//             style: TextStyle(fontSize: 15.sp),
+//           ),
+//         );
+//       },
+//     );
+//   }
+
+//   Future<Map<String, String>> _getFormattedAddresses({
+//     required double fromLatitude,
+//     required double fromLongitude,
+//     required double toLatitude,
+//     required double toLongitude,
+//   }) async {
+//     try {
+//       final fromAddress = await _getAddress(fromLatitude, fromLongitude);
+//       final toAddress = await _getAddress(toLatitude, toLongitude);
+
+//       return {
+//         'fromAddress': fromAddress['address']!,
+//         'fromCity': fromAddress['city']!,
+//         'toAddress': toAddress['address']!,
+//         'toCity': toAddress['city']!,
+//       };
+//     } catch (e) {
+//       throw Exception("فشل في جلب العناوين: $e");
+//     }
+//   }
+
+//   Future<Map<String, String>> _getAddress(
+//       double latitude, double longitude) async {
+//     final address = await GeoCode()
+//         .reverseGeocoding(latitude: latitude, longitude: longitude);
+
+//     if (address == null) {
+//       throw Exception("عنوان غير موجود");
+//     }
+//     String _getCity(String city) {
+//       if (city.isEmpty) return '';
+
+//       List<String> parts = city.split(' ');
+//       if (parts.length > 2) {
+//         return '${parts[0]} ${parts[1]}';
+//       }
+
+//       return city;
+//     }
+
+//     return {
+//       'address': address.streetAddress ?? '',
+//       'city': _getCity(address.city ?? ''),
+//     };
+//   }
+// }
