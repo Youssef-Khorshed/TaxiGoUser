50ee10b2e65acc819a173587e52507a625459f79
AQ <cshazemelwazery@gmail.com>
2024-12-26 13:41:17 +0200

    done

--- lib/Core/app_constants.dart
+++ lib/Core/app_constants.dart
@@ -3,7 +3,8 @@ abstract class AppConstants {
   static String kProfileImage = "kProfileImage";
   static String kName = "kName";
   //65
-
+  static String kTokenValue =
+      "130|pTBzqKJZfluwlnwPXVjOhyL4lQZVpdpg9a0R7lk5317c66ec";
 
   static String kLanguage = "kLanguage";
 }
