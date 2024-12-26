d04ed3ccf68fc86f0d6d89382b9dce776f871087
alaa7041 <a.hassan7041@su.edu.eg>
2024-12-26 14:49:42 +0200

    internet checker

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:taxi_go_user_version/Core/Firebase/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:taxi_go_user_version/Core/Utils/Network/Services/network_connection_checker.dart';
 
 import 'package:taxi_go_user_version/taxi_go_user_edition_app.dart';
 
@@ -17,8 +18,23 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(DevicePreview(
-    enabled: true,
-    builder: (context) => const TaxiGoUserEditionApp(),
-  ));
+   runApp(
+    ValueListenableBuilder(
+      valueListenable: InternetChecker.instance.isConnected,
+      builder: (context, isConnected, child) {
+        return DevicePreview(
+          enabled: true,
+          builder: (context) => isConnected
+              ? const TaxiGoUserEditionApp()
+              : const MaterialApp(
+                  debugShowCheckedModeBanner: false,
+                  title: 'No Internet',
+                  home: Scaffold(
+                    body: Center(child: Text('No Internet Connection')),
+                  ),
+                ),
+        );
+      },
+    ),
+  );
 }
