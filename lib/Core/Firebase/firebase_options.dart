26719a83ee4c6bde3464c407645293ddad6ba088
Youssef-Khorshed <66167521+Youssef-Khorshed@users.noreply.github.com>
2025-02-03 05:12:12 +0200

    Update firebase_options.dart

--- lib/Core/Firebase/firebase_options.dart
+++ lib/Core/Firebase/firebase_options.dart
@@ -50,19 +50,10 @@ class DefaultFirebaseOptions {
   }
 
   static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyA5NDkH74hDUxKMkGegVJDo5bjjry83BGs',
-    appId: '1:36281369878:android:e21d8c2d6aca08de247c37',
-    messagingSenderId: '36281369878',
-    projectId: 'taxi-go-app',
-    storageBucket: 'taxi-go-app.firebasestorage.app',
+   
   );
 
   static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyDJfoKBr1kzAtzMwKiRC5j00kVw3-niKN8',
-    appId: '1:36281369878:ios:071605da6eb04f34247c37',
-    messagingSenderId: '36281369878',
-    projectId: 'taxi-go-app',
-    storageBucket: 'taxi-go-app.firebasestorage.app',
-    iosBundleId: 'com.example.taxiGoUserVersion',
+  
   );
 }
